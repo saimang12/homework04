c3dccd650a0f65591e0023d6261b4afaa298a62d
saimang12@naver.com <saimang12@naver.com>
2023-03-17 12:32:41 +0900

    2023-03-17

--- js/homework04.js
+++ js/homework04.js
@@ -9,18 +9,33 @@ $(function () {
     });
 
     $('.headIcon i:last-child()').on('click', function () {
-        $('.header .gnb').toggleClass('on')
+        $('.header .gnb').toggleClass('on');
+        if ($('.gnb').hasClass('on')) {
+            $('.cover').addClass('on');
+        } else {
+            $('.cover').removeClass('on');
+        };
     });
 
     $('.header .gnb>ul>li>a').on('click', function () {
-        $(this).parent().toggleClass('on')
+        $(this).parent().toggleClass('on');
     });
 
     $('.header,.gnb').on('wheel', function (e) {
         if (window.innerWidth <= 768) {
             e.preventDefault();
         }
-    })
+    });
+
+    $('.cover').on('wheel', function (e) {
+        if (window.innerWidth <= 768) {
+            e.preventDefault();
+        };
+    });
+
+
+
+
 
     $('.mainVisual .arrow').click(function () {
         $('html, body').animate({ scrollTop: 800 }, 500);
